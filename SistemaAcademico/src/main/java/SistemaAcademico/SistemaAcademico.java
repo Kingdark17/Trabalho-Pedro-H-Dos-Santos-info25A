3c7c7432c158a174e2f5b319fe44d7dd28c5c88f
aluno <aluno@lab01-01>
2026-04-07 10:57:53 -0300

    atualizaçao

---
+++ SistemaAcademico/src/main/java/SistemaAcademico/SistemaAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package SistemaAcademico;
+
+/**
+ *
+ * @author aluno
+ */
+public class SistemaAcademico {
+
+    public static void main(String[] args) {
+        
+    }
+}
